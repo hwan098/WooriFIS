7adff012c6baadb40caf27ba7b503ab99ff2a995
Admin <Admin@DESKTOP-BO187LA>
2023-04-26 12:33:35 +0900

    forEach

--- step01_basic/src/step02/array/Array2.java
+++ step01_basic/src/step02/array/Array2.java
@@ -9,17 +9,18 @@ public class Array2 {
 		i[2] = 300;
 		
 		//for
+		System.out.println("--for--");
 		for(int index=0; index < i.length; index++) {
 			System.out.println(i[index]);
 		}
-		
+		System.out.println("--while--");
 		//while
 		int index = 0;
 		while(index < i.length) {
 			System.out.println(i[index]);
 			index++;
 		}
-		
+		System.out.println("--do-while--");
 		//do-while
 		int index2 = 0;
 		do {
@@ -27,7 +28,17 @@ public class Array2 {
 			index2++;
 		}while(index2 < i.length);
 		
-			
+		/*
+		 * forEach
+		 * 배열과 리스트에 사용되는 반복문
+		 * for(단일데이터 : 반복할 데이터){
+		 * 		처리로직
+		 * }	
+		 */
+		System.out.println("--forEach--");
+		for(int v : i) {
+			System.out.println(v);
+		}
 	}
 
 }
